6ec954d1215577d77eab81be2ffc5ad061e4b3cb
exa16003 <joaco553@gmail.com>
2021-06-26 00:39:06 -0300

    assets

--- js/nocturno.js
+++ js/nocturno.js
@@ -14,21 +14,21 @@ MODONOCT.addEventListener("click", (e) => {
 
     if (body.classList == "") {
         body.classList.add("Nocturno");
-        crearGif.src = "/assets/CTA-crear-gifo-modo-noc.svg";
-        logo.src = "/assets/Logo-modo-noc.svg";
-        cam.src = "/assets/camara-modo-noc.svg";
-        film.src = "/assets/pelicula-modo-noc.svg";
-        bntDerecha.src = "/assets/button-slider-right-md-noct.svg"
-        btnIzquierda.src = "/assets/button-slider-left-md-noct.svg"
+        crearGif.src = "assets/CTA-crear-gifo-modo-noc.svg";
+        logo.src = "assets/Logo-modo-noc.svg";
+        cam.src = "assets/camara-modo-noc.svg";
+        film.src = "assets/pelicula-modo-noc.svg";
+        bntDerecha.src = "assets/button-slider-right-md-noct.svg"
+        btnIzquierda.src = "assets/button-slider-left-md-noct.svg"
         MODONOCT.innerHTML="MODO DIURNO"
     } else {
         body.classList.remove("Nocturno");
-        crearGif.src = "/assets/button-crear-gifo.svg";
-        logo.src = "/assets/logo-desktop.svg";
-        cam.src = "/assets/camara.svg";
-        film.src = "/assets/pelicula.svg";
-        bntDerecha.src = "/assets/Button-Slider-right.svg"
-        btnIzquierda.src = "/assets/button-slider-left.svg"
+        crearGif.src = "assets/button-crear-gifo.svg";
+        logo.src = "assets/logo-desktop.svg";
+        cam.src = "assets/camara.svg";
+        film.src = "assets/pelicula.svg";
+        bntDerecha.src = "assets/Button-Slider-right.svg"
+        btnIzquierda.src = "assets/button-slider-left.svg"
         MODONOCT.innerHTML==="MODO NOCTURNO"
         
     }
